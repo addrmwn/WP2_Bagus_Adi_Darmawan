e0f540b602616fc6214c7dbe87bc95ff03b3e5bd
addrmwn <adarmawan107@gmail.com>
2021-09-20 02:51:04 +0700

    fist commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Bagus Adi Darmawan
+ Saya tinggal di daerah pulo jahe
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
