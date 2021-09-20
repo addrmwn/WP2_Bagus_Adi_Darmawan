8f93ef04befd5aecf0c947b84fb290557f66bde0
addrmwn <adarmawan107@gmail.com>
2021-09-20 21:17:39 +0700

    pertemuan4

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Bagus Adi </li>
+ <li>Nama Belakang : Darmawan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Kp pengarengan, jakarta timur, Dki Jakarta</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
